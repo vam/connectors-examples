018b8352a9e254dd56aa8d8b321e1bfbcf4e030b
Vaclav Mikolasek <vaclav.mikolasek@gmail.com>
2017-08-22 19:05:33 +0200

    Created a basic reqres.in example

---
+++ examples/reqres/index.js
@@ -0,0 +1,29 @@
+import { createFrontendConnector, createBackendConnector } from '@crudlio/crudl-connectors-base'
+import { crudToHttp, url, transformData } from '@crudlio/crudl-connectors-base/lib/middleware'
+
+const baseURL = 'https://reqres.in/api'
+
+const base = urlPath => createFrontendConnector(createBackendConnector({ baseURL }))
+    .use(crudToHttp())
+    .use(url(urlPath))
+
+const users = base('users')
+const user = base('users/:id')
+
+// Create a new user
+users.create({ data: { name: 'Vaclav Havel', job: 'President' }})
+    .then((newUser) => {
+        console.log(`User ${newUser.id} created`, newUser)
+    })
+
+// Get user 1
+user(1).read()
+    .then(result => console.log('User with id 1:', result))
+
+// Update user 2
+user(2).update({ data: { role: 'staff' }})
+    .then(result => console.log('User with id 2 was updated:', result))
+
+// Delete user 3
+user(3).delete()
+    .then(result => console.log('User with id 3 was deleted:', result))
